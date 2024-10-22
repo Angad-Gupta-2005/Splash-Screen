dbc21fe46927acda8c2393e0d5c9cc85918d4ca8
Angad Gupta <127626872+Angad-Gupta-2005@users.noreply.github.com>
2024-10-22 23:35:19 +0530

    splash screen is done for 3sec

--- app/src/main/java/com/angad/splashscreen/SplashScreen.kt
+++ app/src/main/java/com/angad/splashscreen/SplashScreen.kt
@@ -1,22 +1,35 @@
 package com.angad.splashscreen
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.angad.splashscreen.databinding.ActivitySplashScreenBinding
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreen : AppCompatActivity() {
+//    creating binding instance
+    private lateinit var binding: ActivitySplashScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+//        Initialised view binding
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+//        Using the Handler for jump to the Main Activity after 3 seconds automatically
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
     }
+
 }
