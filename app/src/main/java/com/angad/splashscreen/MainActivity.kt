6182bab4b6e157eb3ce7012f58f06ce64a691302
Angad Gupta <127626872+Angad-Gupta-2005@users.noreply.github.com>
2024-10-25 18:12:56 +0530

    default alertDialog box is done

--- app/src/main/java/com/angad/splashscreen/MainActivity.kt
+++ app/src/main/java/com/angad/splashscreen/MainActivity.kt
@@ -2,6 +2,7 @@ package com.angad.splashscreen
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.angad.splashscreen.databinding.ActivityMainBinding
 import com.emreesen.sntoast.SnToast
@@ -14,8 +15,8 @@ class MainActivity : AppCompatActivity() {
     //    Creating binding instance
     private lateinit var binding: ActivityMainBinding
 
-//    Creating object of CustomMenu class
-    private val customMenu = com.angad.splashscreen.CustomMenu()
+    //    Creating object of CustomMenu class
+    private val customMenu = CustomMenu()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,9 +35,50 @@ class MainActivity : AppCompatActivity() {
 //        On click menu demo button
         onClickMenuDemoButton()
 
+//        On click show dialog button to display dialog
+        onClickShowDialogButton()
+
     }
 
-//    For menu
+    //    Implementing the alert dialog box in android
+//    For dialog box
+    private fun onClickShowDialogButton() {
+        binding.alertBtn.setOnClickListener {
+            val dialog = AlertDialog.Builder(this)
+            dialog
+                .setTitle("Delete Files")
+                .setMessage(R.string.description)
+                .setIcon(R.drawable.ic_delete)
+                .setPositiveButton("Yes") { _, _ ->
+                    FancyToast.makeText(
+                        this,
+                        "This file has been deleted",
+                        FancyToast.LENGTH_SHORT,
+                        FancyToast.INFO,
+                        false
+                    ).show()
+                }
+                .setNegativeButton("No") { _, _ ->
+                    FancyToast.makeText(
+                        this,
+                        "This file has not been deleted",
+                        FancyToast.LENGTH_SHORT,
+                        FancyToast.INFO,
+                        false
+                    ).show()
+                }
+                .setNeutralButton("Cancel"){ _, _ ->
+                    return@setNeutralButton
+                }
+
+            val alertDialog = dialog.create()
+                alertDialog.setCancelable(false)
+                alertDialog.show()
+
+        }
+    }
+
+    //    For menu
     private fun onClickMenuDemoButton() {
         binding.menuBtn.setOnClickListener {
             customMenu.showMenu(this, it)
@@ -66,7 +108,7 @@ class MainActivity : AppCompatActivity() {
                 // .iconSize(int size) Optional Default: 34dp
                 // .textSize(int size) Optional Default 18sp
                 // .animation(false or true) Optional Default: True
-                 .duration(2000)
+                .duration(2000)
                 // .backgroundColor(R.color.example) Default: It is filled according to the toast type. If an assignment is made, the assigned value is used
                 // .icon(R.drawable.example) Default: It is filled according to the toast type. If an assignment is made, the assigned value is used
                 .build()
